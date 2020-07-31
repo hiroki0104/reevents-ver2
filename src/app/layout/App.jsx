4c92763de7d4c0b9469c4279480cb1e077622a0c
hiroki0104 <hiroki010419@outlook.jp>
2020-07-31 09:47:30 +0900

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EventDashboard from '../../features/events/eventDashBoard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className='App'>
-      <h1>Re-events</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
   );
 }
 
---
+++ src/features/events/eventDashBoard/EventList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import EventListItem from './EventListItem';
+
+function EventList({ events }) {
+  return (
+    <>
+      {events.map((event) => (
+        <EventListItem key={event.id} event={event} />
+      ))}
+    </>
+  );
+}
+
+export default EventList;
